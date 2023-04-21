af54f84e820e24624706f0ab7ac816c77ba05c7b
NATSUMI <natmitv@gmail.com>
2023-04-21 12:05:11 -0500

    css

---
+++ 01 Hola Mundo/app.js
@@ -0,0 +1,12 @@
+const hola=()=>{
+    alert("Hola Mundo");
+    let nombre=prompt("Cuál es tu nombre?");
+    alert("Hola "+nombre+ "  ");
+   
+  }
+  const suma=()=>{
+    let num=parseInt(prompt("Numero 1:   "));
+    let nume=parseInt(prompt("Numero 2:   "));
+    let suma = num+nume;
+    alert("Suma:   "+suma)
+  }
---
+++ 04 Cambiar Imagen/app.js
@@ -0,0 +1,14 @@
+const uno=()=>{
+    let img= document.querySelector("#imagen");
+    
+    img.src="Uno.jpeg";
+    }
+    const dos=()=>{
+    let img= document.querySelector("#imagen");
+    img.src="Dos.jpeg";
+    }
+    const tres=()=>{
+    let img= document.querySelector("#imagen");
+    img.src="Tres.jpeg";
+    
+    }
